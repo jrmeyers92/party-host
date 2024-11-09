5456b81491388b3bc60c96e99a292007b961cf9a
Jake Meyers <jakermeyers92@gmail.com>
2024-11-09 15:45:40 -0600

    add gta to HEAD

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { GoogleTagManager } from "@next/third-parties/google";
 import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
+import Head from "next/head";
 
 import "./globals.css";
 
@@ -55,7 +56,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={GeistSans.className} suppressHydrationWarning>
-      <GoogleTagManager gtmId="G-MXF61DJYTR" />
+      <Head>
+        <GoogleTagManager id="G-MXF61DJYTR" />
+      </Head>
 
       <body className="bg-background text-foreground">
         <ThemeProvider
